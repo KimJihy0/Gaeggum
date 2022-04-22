39248e886333eceb0fcd69f33f4ebd15e659eb5b
박상현 <shawnpark98@naver.com>
2022-04-23 03:15:11 +0900

    Test(findCareer)

--- Gaeggum/Gaeggum/Sources/FindCareer/ViewController/FindCareerViewController.swift
+++ Gaeggum/Gaeggum/Sources/FindCareer/ViewController/FindCareerViewController.swift
@@ -7,11 +7,37 @@
 
 import UIKit
 
-class FindCareerViewController : UIViewController {
+class FindCareerViewController : UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    struct Career { //  test
+        let id : Int
+        let name : String
+    }
+    
+    let data = [Career(id : 1, name : "프론트엔드 개발자"),
+                Career(id : 2, name : "백엔드 개발자"),
+                Career(id : 3, name : "AI 엔지니어"),
+                Career(id : 4, name : "딥러닝 분석가"),
+                ]
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CareerCell", for: indexPath)
+
+        // Configure the cell...
+        cell.textLabel?.text = "cell"
+        cell.textLabel?.text = data[indexPath.row].name
+
+        return cell
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Find Career")
     }
 }
