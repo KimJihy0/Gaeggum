83140933853cbaf338c488b74e72c4d1c3866183
이영석 <2016025732@hanyang.ac.kr>
2022-04-25 03:00:55 +0900

    [feature] ViewController 모델 적용 / #5

--- Gaeggum/Gaeggum/Sources/Home/ViewController/HomeViewController.swift
+++ Gaeggum/Gaeggum/Sources/Home/ViewController/HomeViewController.swift
@@ -8,8 +8,27 @@
 import UIKit
 
 class HomeViewController : UIViewController {
+    @IBOutlet weak var jobNameLabel: UILabel!
+    @IBOutlet weak var jobImageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var taskLabel: UILabel!
+    @IBOutlet weak var curriculumLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Home")
+        
+        updateCareer(career : getTestUser().career!)
+        
+    }
+    
+    func updateCareer(career: Career){
+        jobNameLabel.text = career.name
+        jobImageView.image = UIImage(named: "AIEngineer")
+        taskLabel.text = career.task
+        
+        curriculumLabel.text = career.curriculum
+        tipLabel.text = career.tips
     }
 }
