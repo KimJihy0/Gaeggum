85c26aa2feda034e2381903b6c5a330b704300f7
hyo0508 <2018008168@hanyang.ac.kr>
2022-05-19 22:28:17 +0900

    Extension 위치변경 및 Model 추가

--- Gaeggum/Gaeggum/Models/Project.swift
+++ Gaeggum/Gaeggum/Models/Project.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 struct Project: Codable {
     var title: String
@@ -43,3 +44,65 @@ struct Project: Codable {
     }
 }
 
+extension Project: CustomStringConvertible, Comparable {
+    
+    var term: String {
+        "\(self.startDate.string) ~" + (self.isOnGoing ? "" : "\n\(self.endDate!.string)")
+    }
+    
+    var detailTerm: String {
+        if (self.isOnGoing) {
+            return "\(self.startDate.year)년 \(self.startDate.month)월부터\n진행 중"
+        } else if self.startDate == self.endDate {
+            return  "\(self.startDate.year)년 \(self.startDate.month)월"
+        } else {
+            return "\(self.startDate.year)년 \(self.startDate.month)월부터\n\(self.endDate!.year)년 \(self.endDate!.month)월까지"
+        }
+    }
+    
+    var view: UIView {
+        let projectView = UIView()
+
+        let line = UIView()
+        projectView.addSubview(line)
+        line.backgroundColor = .darkGray
+        line.anchor(top: projectView.topAnchor, left: projectView.leftAnchor, bottom: projectView.bottomAnchor, paddingLeft: 10, width: 2)
+
+        let circle = RoundView()
+        projectView.addSubview(circle)
+        circle.backgroundColor = .white
+        circle.borderWidth = 1
+        circle.borderColor = .darkGray
+        circle.cornerRadius = 3
+        circle.anchor(width: 7, height: 14, centerX: line.centerXAnchor, centerY: projectView.centerYAnchor)
+
+        let termLabel = UILabel()
+        projectView.addSubview(termLabel)
+        termLabel.text = self.term
+        termLabel.textColor = .darkGray
+        termLabel.numberOfLines = 0
+        termLabel.anchor(top: projectView.topAnchor, left: line.rightAnchor, bottom: projectView.bottomAnchor, paddingTop: 10, paddingLeft: 15, paddingBottom: 10, width: 90)
+
+        let contentLabel = UILabel()
+        projectView.addSubview(contentLabel)
+        contentLabel.text = self.title
+        contentLabel.numberOfLines = 0
+        contentLabel.anchor(top: projectView.topAnchor, left: termLabel.rightAnchor, bottom: projectView.bottomAnchor, right: projectView.rightAnchor, paddingTop: 10, paddingLeft: 10, paddingBottom: 10)
+
+        return projectView
+    }
+    
+    var description: String {
+        return """
+            title: \(self.title)
+            term: \(self.startDate.string) ~ \(isOnGoing ? "" : self.endDate!.string)
+            content:
+            \(content)
+            """
+    }
+    
+    static func < (lhs: Project, rhs: Project) -> Bool {
+        return lhs.startDate < rhs.startDate || (lhs.startDate == rhs.startDate && lhs.endDate ?? YearMonth() < rhs.endDate ?? YearMonth())
+    }
+
+}
