1cf8c0d6d864ffe3b5d7df006ea14bc3d8904f8b
이영석 <2016025732@hanyang.ac.kr>
2022-05-24 02:31:27 +0900

    [refactor] model : delete Education, modify UserInfo, add Question / #4

---
+++ Gaeggum/Gaeggum/Models/Question.swift
@@ -0,0 +1,13 @@
+//
+//  Question.swift
+//  Gaeggum
+//
+//  Created by zeroStone ⠀ on 2022/05/24.
+//
+
+import Foundation
+
+struct Question {
+    let question: String
+    let answer: [(String, Stat)]
+}
