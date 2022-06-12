6449583db65ce6b14d7afe5a0cdd638f33daf8fe
이영석 <2016025732@hanyang.ac.kr>
2022-06-13 01:03:39 +0900

    feat: update question ui / #59

---
+++ Gaeggum/Gaeggum/Sources/Home/ViewController/InitialTestViewController.swift
@@ -0,0 +1,12 @@
+//
+//  InitialTestViewController.swift
+//  Gaeggum
+//
+//  Created by zeroStone ⠀ on 2022/06/09.
+//
+
+import UIKit
+
+class InitialTestViewController: UIViewController {
+    
+}
