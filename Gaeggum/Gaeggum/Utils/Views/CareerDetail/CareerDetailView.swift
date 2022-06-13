d8c2f4fec424f5f6696663d0021d9123aad5bed0
이영석 <2016025732@hanyang.ac.kr>
2022-06-13 15:14:01 +0900

    feat: add chart library

--- Gaeggum/Gaeggum/Utils/Views/CareerDetail/CareerDetailView.swift
+++ Gaeggum/Gaeggum/Utils/Views/CareerDetail/CareerDetailView.swift
@@ -7,8 +7,11 @@
 
 
 import UIKit
+import Charts
 
 class CareerDetailView: UIView {
+    
+    @IBOutlet weak var chartView: RadarChartView!
     
     @IBOutlet weak var jobNameLabel: UILabel!
     @IBOutlet weak var jobImageView: UIImageView!
@@ -20,6 +23,27 @@ class CareerDetailView: UIView {
     @IBOutlet weak var curriculumLabel: UILabel!
     @IBOutlet weak var tipLabel: UILabel!
 
+    // 차트 라이브러리 관련 변수
+    let greenDataSet = RadarChartDataSet(
+        entries: [
+            RadarChartDataEntry(value: 5),
+            RadarChartDataEntry(value: 4),
+            RadarChartDataEntry(value: 1),
+            RadarChartDataEntry(value: 1),
+            RadarChartDataEntry(value: 2)
+        ]
+    )
+    
+    let blueDataSet = RadarChartDataSet(
+        entries: [
+            RadarChartDataEntry(value: 3),
+            RadarChartDataEntry(value: 5),
+            RadarChartDataEntry(value: 1),
+            RadarChartDataEntry(value: 2),
+            RadarChartDataEntry(value: 1)
+        ]
+    )
+    
     let nibName = "CareerDetail"
     var abilityList : [Ability] = []
     
@@ -83,6 +107,38 @@ class CareerDetailView: UIView {
         tipLabel.numberOfLines = 0
         tipLabel.lineBreakMode = .byCharWrapping
         tipLabel.text = career.tips
+        
+        updateChart()
+    }
+
+    
+    func updateChart() {
+        let data = RadarChartData(dataSets: [greenDataSet, blueDataSet])
+
+        greenDataSet.colors = [.green]
+        greenDataSet.drawValuesEnabled = false
+        greenDataSet.lineWidth = 2.0
+        blueDataSet.colors = [.blue]
+        blueDataSet.drawValuesEnabled = false
+        blueDataSet.lineWidth = 2.0
+        
+        chartView.data = data
+        chartView.legend.enabled = false
+        chartView.yAxis.axisMinimum = 0
+        chartView.yAxis.axisMaximum = 5
+        chartView.drawWeb = true
+        chartView.webLineWidth = 0
+        chartView.innerWebColor = .lightGray
+        chartView.innerWebLineWidth = 1.0
+        chartView.rotationEnabled = false
+        chartView.highlightPerTapEnabled = false
+        chartView.xAxis.labelFont = .systemFont(ofSize: 10)
+        chartView.xAxis.valueFormatter = XAxisFormatter()
+        
+        chartView.yAxis.drawLabelsEnabled = false
+        
+        
+        
     }
 }
 
@@ -117,5 +173,14 @@ extension CareerDetailView: UICollectionViewDelegate, UICollectionViewDataSource
         cell.tagLabel.textColor = UIColor.white
         
         return cell
+    }
+}
+
+class XAxisFormatter: AxisValueFormatter {
+    
+    let titles = ["모델 적용력", "끈기와 인내", "트렌디한 논문 습득력", "수학적 지식", "코딩 능력"]
+    
+    func stringForValue(_ value: Double, axis: AxisBase?) -> String {
+        titles[Int(value)]
     }
 }
